220814e2898a083c62a10e5a2c511c3efe6fdbd8
Marrollar <>
2025-04-04 18:03:58 -0400

    Color formatting for cluster titles to make it easier to tell which aspect they belong to

--- client/src/types.tsx
+++ client/src/types.tsx
@@ -6,10 +6,18 @@ export enum Aspects {
     Inertia = "Inertia"
 }
 
-export const AspectColors: { [id in Aspects]: string } = {
+export const Aspect_BG_Colors: { [id in Aspects]: string } = {
     Force: "#280505",
     Life: "#282805",
     Entropy: "#280028",
     Form: "#052805",
     Inertia: "#052828"
+}
+
+export const Aspect_Txt_Color: { [id in Aspects]: string } = {
+    Force: "#e67f7f",
+    Life: "#c1a757",
+    Entropy: "#beb3ff",
+    Form: "#4ae88c",
+    Inertia: "#73b3f2"
 }
